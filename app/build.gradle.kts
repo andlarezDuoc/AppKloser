1e398018f651d3224893061855296c6dd355ac17
Andrea <and.larez@duocuc.cl>
2025-11-27 23:02:26 -0300

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "cl.duoc.amigo"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "cl.duoc.amigo"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
