0070031d9bb5a2c6eba6810b195584587bed82e5
Andrea <and.larez@duocuc.cl>
2025-12-15 14:28:52 -0300

    Solventado errores

--- app/src/main/java/cl/duoc/kloser/data/model/FormularioModel.kt
+++
@@ -1,8 +0,0 @@
-package cl.duoc.amigo.model
-
-data class FormularioModel(
-    var nombre: String = "",
-    var correo: String = "",
-    var contrasena: String = "",
-    var terminos: Boolean = false
-)
--- app/src/main/java/cl/duoc/kloser/data/model/MensajeError.kt
+++
@@ -1,8 +0,0 @@
-package cl.duoc.amigo.model
-
-data class MensajesError(
-    var nombre: String = "",
-    var correo: String = "",
-    var contrasena: String = "",
-    var terminos: String = ""
-)
